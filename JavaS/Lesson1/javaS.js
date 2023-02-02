34f097116fcc9ae413f4d817346688c1e9c50477
YuraKachur <yuri.kachur@gmail.com>
2023-02-02 17:05:11 +0200

    Lesson1

--- JavaS/Lesson1/javaS.js
+++ JavaS/Lesson1/javaS.js
@@ -1,62 +1,75 @@
-let a = 'hello';
-let b = 'owu';
-let c = 'com';
-let d = 'ua';
-let e = 1;
-let f = 10;
-let g = -999;
-let j = 123;
-let p = 3.14;
-let w = 2.7;
-let r = 16;
-let t = true;
-let y = false;
+//- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let j = 123;
+// let p = 3.14;
+// let w = 2.7;
+// let r = 16;
+// let t = true;
+// let y = false;
 
 
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(j);
+// console.log(p);
+// console.log(w);
+// console.log(r);
+// console.log(t);
+// console.log(y);
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
-console.log(f);
-console.log(g);
-console.log(j);
-console.log(p);
-console.log(w);
-console.log(r);
-console.log(t);
-console.log(y);
 
-let name = ' Yura';
-let SecondNmae = ' Kchur';
-let  LastName = ' Genrichovych'
+//- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 
-let person = name + SecondNmae + LastName;
+// let name = ' Yura';
+// let SecondNmae = ' Kchur';
+// let  LastName = ' Genrichovych'
+//
+// let person = name + SecondNmae + LastName;
+//
+// console.log(person);
 
-console.log(person);
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
 
-let aa = 100;
-let bb = '100';
-let cc = true;
+// let aa = 100;
+// let bb = '100';
+// let cc = true;
+//
+// let ques1 = 'Write your name'
+// let ques2= 'Write your surname'
+// let ques3 = 'Write your age'
+//
+// console.log(typeof(aa));
+//
+// console.log(typeof(bb));
+//
+// console.log(typeof(cc));
 
-let ques1 = 'Write your name'
-let ques2= 'Write your surname'
-let ques3 = 'Write your age'
 
-console.log(typeof(aa));
+//Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
 
-console.log(typeof(bb));
 
-console.log(typeof(cc));
-
-let beka = prompt(ques1);
-let bek =  prompt(ques2);
-let be  =   prompt(ques3);
-
-console.log(beka);
-console.log(bek);
-console.log(be);
+// let beka = prompt(ques1);
+// let bek =  prompt(ques2);
+// let be  =   prompt(ques3);
+//
+// console.log(beka);
+// console.log(bek);
+// console.log(be);
 
 
 
