80c39c4c215efcf7c3a7cd38f4a6c4364f1e0c87
YuraKachur <yuri.kachur@gmail.com>
2023-02-02 16:48:57 +0200

    Lesson1

---
+++ JavaS/Lesson1/javaS.js
@@ -0,0 +1,83 @@
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let j = 123;
+let p = 3.14;
+let w = 2.7;
+let r = 16;
+let t = true;
+let y = false;
+
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(j);
+console.log(p);
+console.log(w);
+console.log(r);
+console.log(t);
+console.log(y);
+
+let name = ' Yura';
+let SecondNmae = ' Kchur';
+let  LastName = ' Genrichovych'
+
+let person = name + SecondNmae + LastName;
+
+console.log(person);
+
+let aa = 100;
+let bb = '100';
+let cc = true;
+
+let ques1 = 'Write your name'
+let ques2= 'Write your surname'
+let ques3 = 'Write your age'
+
+console.log(typeof(aa));
+
+console.log(typeof(bb));
+
+console.log(typeof(cc));
+
+let beka = prompt(ques1);
+let bek =  prompt(ques2);
+let be  =   prompt(ques3);
+
+console.log(beka);
+console.log(bek);
+console.log(be);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
