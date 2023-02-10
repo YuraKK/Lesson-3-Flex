9b103841e09dc8e8a1a88c2f1b2e5112ff8c8da3
YuraKachur <yuri.kachur@gmail.com>
2023-02-10 23:21:42 +0200

    Lesson2

--- JavaS/Lesson2/jAVAs.js
+++ JavaS/Lesson2/jAVAs.js
@@ -84,91 +84,146 @@ console.log(client);
 //     - Є змінна х, якій ви надаєте довільне числове значення.
 //     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
 // (в першу, другу, третю или четверту частину години).
-let x = -3;
-
-if (x!==0){
-    console.log('Єсссс');
-}
-else  {
-    console.log('Не вірно');
-}
+// let x = -3;
+//
+// if (x!==0){
+//     console.log('Єсссс');
+// }
+// else  {
+//     console.log('Не вірно');
+// }
 // - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 
-let time = 61;
-if (time<=3)
-{
-    console.log('Перша частина');
-}
-else if  (time>=4 && time<=6)
-{
-    console.log('Друга група');
-}
-else if (time>=7 && time<=9)
-{
-    console.log('Третя група');
-}
-else if (time>=10 && time<=12) {
-    console.log("Четверта група");
-}
-else {
-    console.log('Забагато');
-}
+// let time = 61;
+// if (time<=3)
+// {
+//     console.log('Перша частина');
+// }
+// else if  (time>=4 && time<=6)
+// {
+//     console.log('Друга група');
+// }
+// else if (time>=7 && time<=9)
+// {
+//     console.log('Третя група');
+// }
+// else if (time>=10 && time<=12) {
+//     console.log("Четверта група");
+// }
+// else {
+//     console.log('Забагато');
+// }
 
 
 
 // - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
-
-let day = 32;
-
-if (day<=10) {
-
-    console.log('Першу');
-}
-else if (day <=20) {
-    console.log('Другу');
-}
-else if (day<=31) {
-    console.log('Третю');
-}
-else {
-    console.log('Завелике число');
-}
+//
+// let day = 32;
+//
+// if (day<=10) {
+//
+//     console.log('Першу');
+// }
+// else if (day <=20) {
+//     console.log('Другу');
+// }
+// else if (day<=31) {
+//     console.log('Третю');
+// }
+// else {
+//     console.log('Завелике число');
+// }
 
 // - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
 //     - Користувач вводить або має два числа.
 //         Потрібно знайти та вивести максимальне число з тих двох .
 //         Також потрібно врахувати коли введені рівні числа.
-let week = 'Sunday'
-switch (week){
-
-    case "Monday":
-        console.log("Рома прийшов");
-        break;
-    case 'Thusday':
-        console.log('Чистити зубки');
-        break;
-    case 'Wednesday':
-        console.log('Мити ноги');
-        break;
-    case 'Thursday':
-        console.log('Чистити вуха');
-        break;
-    case 'Friday':
-        console.log('Прати речі');
-        break;
-    case 'Saturday':
-        console.log('Допомогти мамі');
-        break;
-    case 'Sunday':
-        console.log('Допомогти татові');
-        break;
-
-
-}
+// let week = 'Sunday'
+// switch (week){
+//
+//     case "Monday":
+//         console.log("Рома прийшов");
+//         break;
+//     case 'Thusday':
+//         console.log('Чистити зубки');
+//         break;
+//     case 'Wednesday':
+//         console.log('Мити ноги');
+//         break;
+//     case 'Thursday':
+//         console.log('Чистити вуха');
+//         break;
+//     case 'Friday':
+//         console.log('Прати речі');
+//         break;
+//     case 'Saturday':
+//         console.log('Допомогти мамі');
+//         break;
+//     case 'Sunday':
+//         console.log('Допомогти татові');
+//         break;
+//
+//
+// }
 
 
 //     - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 //         за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
 
+// let x = 'fff'
+
+
 //     - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
-//
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4} ]
+
+if (coursesAndDurationArray[0].monthDuration<=5) {
+    console.log('Not bad');
+
+}
+
+else {
+    console.log('Супер');
+}
+if (coursesAndDurationArray[1].monthDuration<=5) {
+    console.log('Not bad');
+
+}
+else {
+    console.log('Супер');
+}
+if (coursesAndDurationArray[2].monthDuration<=5) {
+    console.log('Not bad');
+
+}
+else {
+    console.log('Супер');
+
+}
+if (coursesAndDurationArray[3].monthDuration<=5) {
+    console.log('Not bad');
+}
+else {
+    console.log('Супер');
+}
+
+if (coursesAndDurationArray[4].monthDuration<=5) {
+    console.log('Not bad');
+
+}
+else {
+        console.log('Супер');
+    }
+if (coursesAndDurationArray[5].monthDuration<=5) {
+    console.log('Not bad');
+
+
+}
+else {
+    console.log('Супер');
+}
